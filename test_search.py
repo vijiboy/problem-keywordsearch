79fe1712162c7691ec38fc8fff0e15468c665a3b
vijiboy <more.vijay@gmail.com>
2020-09-06 20:23:55 +0530

    minor: improved test intent

--- test_search.py
+++ test_search.py
@@ -5,9 +5,14 @@ import os
 class Test_TextStructuring(unittest.TestCase):
     def test_ConvertParagraph_To_ListOfLists(self):
         #print(ConvertParagraph_To_ListOfLists("first sentence. and second.  "))
-        sentences = ConvertParagraph_To_ListOfLists("first sentence. and second. ")
-        self.assertEqual(sentences[0], ['first', 'sentence'])
-        self.assertEqual(sentences[1], ['and', 'second'])
+        for text in [ "first sentence. and second. ",
+                      "\nfirst sentence. and second. ",
+                      "   first sentence.   and second.   ",
+                      ". first sentence. and second.  " ] :
+            sentences = ConvertParagraph_To_ListOfLists(text)
+            self.assertEqual(sentences[0], ['first', 'sentence'])
+            self.assertEqual(sentences[1], ['and', 'second'])
+
 
 
 if __name__ == '__main__':
