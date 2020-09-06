40b89faccf4162b9a7cdefea3835c942c05856bc
vijiboy <more.vijay@gmail.com>
2020-09-07 00:07:41 +0530

    in-progess: keyword search

--- findSentences.py
+++ findSentences.py
@@ -1,10 +1,27 @@
-def ConvertParagraph_To_ListOfLists(text):
+def ConvertParagraphToSentenceLists(text):
+    ''' parse and convert text to sentence lists '''
     if not text or not isinstance(text, str) or not text.strip():
         raise ValueError("invalid or empty text")
-    textSentences = []
+    SentenceLists = []
+    sentenceEnd = ". " # sentence defined as string ending with dot then space
     for para in (text.splitlines()):
-        for sentence in para.split(". "):
+        for sentence in para.split(sentenceEnd): 
             if not sentence.strip(): continue
-            textSentences.append(sentence.split())
-    return textSentences
-                                
+            SentenceLists.append(sentence.split())
+    return SentenceLists
+
+def BuildSearchSetFromSentenceLists(SentenceLists):
+    ''' builds a map (Hitting Set) of words to their sentences '''
+    wordSearchSet = {} # maps word to sentences its present in
+    for sindex, sentence in enumerate(SentenceLists):
+        for word in sentence:
+            if word not in wordSearchSet: wordSearchSet[word] = set()
+            wordSearchSet[word].add(sindex)
+    return wordSearchSet
+
+def getSentencesSetsFromKeywords(keywords, wordSearchSet):
+    matchedIndexes = []
+    for word in keywords:
+        matchedIndexes.append(
+            set() if word not in wordSearchSet else wordSearchSet[word] 
+    return matchedIndexes
--- test_search.py
+++ test_search.py
@@ -3,17 +3,45 @@ from findSentences import *
 import os
 
 class Test_TextStructuring(unittest.TestCase):
-    def test_ConvertParagraph_To_ListOfLists(self):
-        #print(ConvertParagraph_To_ListOfLists("first sentence. and second.  "))
+    def test_ConvertParagraph_To_SentenceLists(self):
+        #print(ParseParagraph("single sentence"))
+        sentences = ConvertParagraphToSentenceLists("single sentence")
+        self.assertEqual(len(sentences), 1)
+        self.assertEqual(sentences[0], ['single', 'sentence'])
+
         for text in [ "first sentence. and second. ",
                       "\nfirst sentence. and second. ",
+                      "first sentence. and second. \n",
                       "   first sentence.   and second.   ",
                       ". first sentence. and second.  " ] :
-            sentences = ConvertParagraph_To_ListOfLists(text)
+            sentences = ConvertParagraphToSentenceLists(text)
+            self.assertEqual(len(sentences), 2)
             self.assertEqual(sentences[0], ['first', 'sentence'])
             self.assertEqual(sentences[1], ['and', 'second'])
 
+    def test_InvalidParagraph_Raises_ValueError(self):
+        with self.assertRaises(ValueError):
+            ConvertParagraphToSentenceLists("")
+        with self.assertRaises(ValueError):
+            ConvertParagraphToSentenceLists("\n")
 
+    def test_ConvertSentencesToSet(self):
+        searchSet = BuildSearchSetFromSentenceLists(
+            ConvertParagraphToSentenceLists(
+                "first line. second line"))
+        self.assertEqual(searchSet, {'first':{0}, 'line':{0,1}, 'second': {1}})
+
+    def test_RetriveSentences_WithMaxMatchesFirst(self):
+        slist = ConvertParagraphToSentenceLists(
+            "lion is a wild animal. dog is a pet animal")
+        kwords = BuildSearchSetFromSentenceLists(
+            slist)
+        keywordSearchResult = getSentencesSetsFromKeywords(
+            'what is a wild animal'.split(),
+            kwords)
+        print(keywordSearchResult)
+
+        
 
 if __name__ == '__main__':
     unittest.main()
